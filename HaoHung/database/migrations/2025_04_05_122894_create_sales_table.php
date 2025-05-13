e41960c00daac4e27a435de727bdbd5fbc105fef
nguyenhuuhaohung <nguyenhaohung640@gmail.com>
2025-05-13 16:48:42 +0700

    create file

---
+++ HaoHung/database/migrations/2025_04_05_122894_create_sales_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('sales', function (Blueprint $table) {
+            $table->id('ID_Sale');
+            $table->string('Name_Sale');
+            $table->timestamp('Start_Date')->useCurrent();
+            $table->timestamp('End_Date')->useCurrent();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('sales');
+    }
+};
