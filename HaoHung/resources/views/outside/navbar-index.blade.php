080eae26003023c8b8e38b4ba422f8211b3c2cd4
nguyenhuuhaohung <nguyenhaohung640@gmail.com>
2025-05-13 19:09:23 +0700

    create file

---
+++ HaoHung/resources/views/outside/navbar-index.blade.php
@@ -0,0 +1,22 @@
+
+
+  <!-- Navbar -->
+  <nav class="navbar">
+    <ul class="nav-list">
+      <li><a href="#">Home</a></li>
+      <li><a href="#">Categories</a></li>
+      <li><a href="#">Make an Appointment</a></li>
+      <li><a href="#">Shopping Cart</a></li>
+      <li><a href="#">List Car</a></li>
+      <li><a href="#">Customer Support</a></li>
+      <li><a href="#">Introduce</a></li>
+      <li>
+        <input type="text" placeholder="Search...">
+        <button>🔍</button>
+      </li>
+    </ul>
+  </nav>
+
+  <!-- Slider -->
+  
+
